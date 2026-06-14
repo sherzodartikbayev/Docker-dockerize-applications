9d2057b127d27eab82a03b60ba2fa232d44796d8
Sherzod <info@web-pro.uz>
2026-06-14 13:40:03 +0500

    first commit

---
+++ mern-docker/backend/index.js
@@ -0,0 +1,11 @@
+require("dotenv");
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("Hello"));
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
